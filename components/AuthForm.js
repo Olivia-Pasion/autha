5d174e6973dca612c3171f7af856f9d619688c4b
olivia pasion <oap9820@gmail.com>
2022-06-27 14:05:20 -0700

    completed

--- components/AuthForm.js
+++ components/AuthForm.js
@@ -6,6 +6,11 @@ export default function createAuthForm(form, { handleAuth }) {
         e.preventDefault();
 
         // *** make form data and call handleAuth with email and password
+        const formData = new FormData(form);
+        handleAuth(
+            formData.get('email'),
+            formData.get('password')
+        );
     });
 
     return () => { };
